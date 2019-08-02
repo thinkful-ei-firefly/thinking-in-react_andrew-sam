639d64eb627146b53635350656610ea1176c78c0
Sam Walker <jack.nasal@gmail.com>
2019-08-02 11:28:08 -0400

    changes to part

--- src/components/Part.js
+++ src/components/Part.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+const options = this.props.features[key].map((item, index) => {
+  const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
+  const featureClass = 'feature__option ' + selectedClass;
+  return <li key={index} className="feature__item">
+    <div className={featureClass}
+      
+      onClick={e => this.updateFeature(key, item)}>
+        { item.name }
+        ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+          .format(item.cost) })
+    </div>
+  </li>
+});
+
 function Part() {
   return (
     <div /> 
